a490c90488e20156e76e8d418d0b01543dd729de
Paul-Michael Agapow <github@agapow.net>
2009-04-16 15:14:27 +0100

    Release 0.3 (public release, docs, convalign)

---
+++ bioscripts/convert/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Biopython scripts for converting molecular sequences.
+
+Bioinformatics is bedevilled by a large number of file formats. Biopython 
+provides classes and IO functions that allow interconversion. This module
+provides scripts that use Biopython to simply convert multiple files on the
+commandline.
+
+This module is not intended for importing, but the module and egg structure
+make for simple distribution and checking of prerequisites for scripts. The
+scripts may be installed seperately.
+
+"""
+
+__docformat__ = 'restructuredtext en'
+__version__ = "0.3.1"
+__author__ = 'Paul-Michael Agapow <agapow@bbsrc.ac.uk>'
+
+
+### IMPORTS ###
+
+## CONSTANTS & DEFINES ###
+
+### TEST & DEBUG ###
+
+### MAIN ###
+
+if __name__ == '__main__':
+	_doctest()
+
+
+### END ########################################################################
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+from bioscripts.convert import __version__
+
+setup (
+	name='bioscripts.convert',
+	version=__version__,
+	description="Biopython scripts for converting molecular sequences.",
+	long_description=open("README.txt").read() + "\n" +
+	open(os.path.join("docs", "HISTORY.txt")).read(),
+	# Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Environment :: Console",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: BSD License",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		"Programming Language :: Python",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Text Processing",
+	],
+	keywords='bioinformatics conversion',
+	author='Paul-Michael Agapow',
+	author_email='agapow@bbsrc.ac.uk',
+	url='http://www.agapow.net/software/bioscripts.convert',
+	license='BSD',
+	packages=find_packages(exclude=['ez_setup']),
+	namespace_packages=['bioscripts'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'setuptools',
+		'biopython >= 1.49',
+	],
+	entry_points={
+		'console_scripts': [
+			'convbioseq = bioscripts.convert.convbioseq:main',
+			'convalign = bioscripts.convert.convalign:main',
+		],
+	},
+	#scripts=[
+	#	'bioscripts/convert/convbioseq.py',
+	#],
+)
